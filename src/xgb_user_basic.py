261ad99ffc5a00064976feca02a4cb7cf6fc10b2
Tongzhenguo <tongzhenguotongzhenguo@gmail.com>
2017-12-20 18:35:56 +0800

    feat:update

--- src/xgb_user_basic.py
+++ src/xgb_user_basic.py
@@ -7,3 +7,8 @@ userProfile_train['gender'] = userProfile_train['gender'].fillna('-1') #3
 userProfile_train['province'] = userProfile_train['province'].fillna('-1') #-1,34
 userProfile_train['age'] = userProfile_train['age'].fillna('-1') #-1,60后-90后,00后
 
+gender = pd.get_dummies( userProfile_train['gender'],prefix='gender' )
+province = pd.get_dummies( userProfile_train['province'],prefix='province' )
+age = pd.get_dummies( userProfile_train['age'],prefix='age' )
+tr_x = pd.concat([gender,province,age],axis=1,join='inner') #axis=1 是行
+print( tr_x.head(3) )
